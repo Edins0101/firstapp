51dff0acd5d3aaa77b86af21cbde639f029c63c8
= <=>
2023-12-07 14:37:06 -0500

    Directivas

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
-  <h1> Contador </h1>
-  <CounterComposition />
-  <CounterComposition />
-  <CounterComposition />
+  <h1> Paises </h1>
+  <ExampleFor />
 </template>
 
 <script lang="ts" setup>
 //import PostContainer from './components/PostContainer.vue';
 //import CounterOption from './components/CounterOption.vue';
-import CounterComposition from './components/CounterComposition.vue';
+//import CounterComposition from './components/CounterComposition.vue';
+//import ExampleSelective from './components/ExampleSelective.vue'
+import ExampleFor from './components/ExampleFor.vue';
+
+
 </script>
 
 <style>
 h1{
-  color: green;
+  color: black;
 }
 </style>
---
+++ src/components/ExampleSelective.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1 v-if="age>=18">Eres mayor de Edad</h1>
+    <h1 v-else>Eres menor de Edad</h1>
+</template>
+<script lang="ts" setup>
+    let age = 10
+
+</script>
+<style scoped>
+
+</style>
